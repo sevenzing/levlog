314b315cfe056659da6b3812746f87781bdf358b
Lev Lymarenko <lymarenko.lev@gmail.com>
2021-08-15 23:09:22 +0300

    ! .env file

--- levlog/settings.py
+++ levlog/settings.py
@@ -12,13 +12,20 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import environ
+import os
 
+
+env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-env = environ.Env()
-env.read_env(open(BASE_DIR / '.env'))
+# Env file
+
+ENV_FILE = BASE_DIR / '.env'
+
+if os.path.exists(ENV_FILE):
+    env.read_env(open(BASE_DIR / '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
