d3fc01933f1b3ded97cdbc45a43ed139907196f4
Lev Lymarenko <lymarenko.lev@gmail.com>
2021-08-26 13:43:50 +0300

    + category icon

--- blog/models/category.py
+++ blog/models/category.py
@@ -1,20 +1,13 @@
-from django.conf import settings
+
 from django.db import models
-from django.db.models import Q
-from django.urls import reverse
 from django.utils.translation import gettext as _
-from tinymce.models import HTMLField
-from stdimage import StdImageField
+
 
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
-    image = StdImageField(
+    image_svg = models.FileField(
         upload_to='category_images/', 
-        null=True,
-        variations={
-            'small' : {'width': 64, 'height': 64}
-        },
         verbose_name=_('image'),
     )
 
